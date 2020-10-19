eb847736fc8d359ad3433d02ab562df2c8c8724d
Martin Grellmann <martin@grellmann.eu>
2020-10-19 23:02:22 +0200

    Change the Checking if the path in Routing as a callable closure

--- src/Router.php
+++ src/Router.php
@@ -46,17 +46,47 @@ class Router
     }
 
     /**
+     * Get the Parameter
+     * @return mixed the in $this->request held Parameter from the url
+     */
+    public function getRequest() {
+        // Get the given Parameter from the url, hold in $this->request
+        return $this->request;
+    }
+
+    /**
+     * Check the rout for a callable closure
+     * @return bool Give a true, if the path has a callable closure, or a false, if not.
+     */
+    public function checkClosure() {
+        // check if Route exist, when yes, call the callable closure and put the value of the runner to yes
+        $runner = false;
+        foreach ($this->routes as $key=>$value) {
+            unset($value);
+            // Define th runner with tru, when a closure to a path exist
+            if($key === $this->getRequest()) {
+                $runner = true;
+            }
+        }
+        return $runner;
+    }
+
+    /**
      * Execute the in the instanced class specified routes
      */
     public function execute() {
-        // Get the given Parameter from the url, hold in $this->request
-        $path = DIRECTORY_SEPARATOR.$this->request;
-        // if Route not exist, give a error404
-        if($this->routes[$path] !== null) {
-            $this->routes[$path]();
-        } else {
+        // When the runner has true, give back the callable closure, when not, give a 404
+        if($this->checkClosure() === false) {
             $message = new Mssg();
             $message->error404();
+        } else {
+            $this->routes[$this->getRequest()]();
         }
+        unset($runner);
+
+
+
+//
+
     }
 }
